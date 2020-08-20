9578c3c836ac9b1038101c2b2938ff258a41c7e1
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-20 15:28:37 -0400

    Filling out Form.js, finished text inputs: name, email, pass. working on radio buttons: Terms of conditions

--- src/App.js
+++ src/App.js
@@ -30,24 +30,49 @@ const initialDisabled = true;
 function App() {
   ///// STATES /////
   const [users, setUsers] = useState(initialUsers);    // array of user objects
-  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formValues, setFormValues] = useState(initialFormValues);    // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  ///// FORM ACTIONS /////
+  const inputChange = (name, value) => {
+  
+  }
+
+  const submit = () => {
+
+  }
+
+
+  ///// SIDE EFFECTS /////
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/*<p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>*/}
+        <p>
+          Advanced Form Management
         </p>
-        <a
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <Form
+        values={formValues}
+        errors={formErrors}
+        disabled={disabled}
+        inputChange={inputChange}
+        submit={submit}
+      />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,70 @@ import React from 'react'
 
 
 
-export default function Form() {
-    return null
+export default function Form(props) {
+    const { values, errors, disabled, inputChange, submit } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
+    const onInputChange = evt => {
+        const { name, value } = evt.target;
+        inputChange(name, value);
+    }
+
+    return(
+        <form className='form container' onSubmit={onSubmit}>
+            <div classNam='form-group submit'>
+                <h2>Onboard a new user</h2>
+
+                {/* button starts out disabled untill all parameters filled */}
+                <button disabled={disabled}>submit</button>
+
+                <div className='errors'>
+                    {/* to render validation errors here */}
+                    <div>{errors.name}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                    <div>{errors.terms}</div>
+                </div>
+            </div>
+
+            <div className='form-group inputs'>
+                <h4>Please input the name, email, and password for the User</h4>
+
+                {/* ///// TEXT INPUTS ///// */}
+                <label>Name&nbsp;
+                    <input
+                        value={values.name}
+                        onChange={onInputChange}
+                        name='name'
+                        type='text'
+                    />
+                </label>
+
+                <label>Email&nbsp;
+                    <input
+                        value={values.email}
+                        onChange={onInputChange}
+                        name='email'
+                        type='text'
+                    />
+                </label>
+
+                <label>Password&nbsp;
+                    <input
+                        value={values.password}
+                        onChange={onInputChange}
+                        name='password'
+                        type='text'
+                    />
+                </label>
+
+                {/* ///// RADIO BUTTONS ///// */}
+            </div>
+        </form>
+    )
 }
 
