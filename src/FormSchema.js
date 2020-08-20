c408955042159853cfda1e75e7c91b87f9a53b24
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-20 16:06:09 -0400

    Forgot to branch, prior commits: setup the workplace, added functionality to App.js, Fleshed out Form.js and User.js; working on FormSchema.js and will finish out with completing App.js

--- src/FormSchema.js
+++ src/FormSchema.js
@@ -4,6 +4,13 @@ const formSchema = yup.object().shape({
     name: yup
         .string()
         .min(2, 'Name must be at least 3 characters long')
-        .required('Name is required')
-        
+        .required('Name is required'), 
+    email: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('Must include email address'),
+    password: yup
+        .string()
+        .min(8, 'Password must be at least 8 characters long')
+        .matches[]
 })
