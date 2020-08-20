63fc68bcb772aa17edaa0cb7adccb2ab7d1e87f6
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-20 14:56:10 -0400

    Created initial values and some states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// get imports 
+import Form from './Form'
+import axios from 'axios'
+import * as yup from 'yup'
+
+///// INITIAL STATES /////
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  email: '',
+  password: '',
+  ///// RADIO BUTTONS /////
+  terms: '',
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialUsers = []
+const initialDisabled = true;
+
 function App() {
+  ///// STATES /////
+  const [users, setUsers] = useState(initialUsers);    // array of user objects
+  const [formValues, setFormValues] = useState(initialFormValues);
+
   return (
     <div className="App">
       <header className="App-header">
